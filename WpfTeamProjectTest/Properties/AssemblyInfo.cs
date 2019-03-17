4b7d7d2f901f5a75adc4d7322d4d5eef321b9995
lindeansammy <45698819+lindeansammy@users.noreply.github.com>
2019-03-17 15:52:10 -0400

    Add files via upload

---
+++ WpfTeamProjectTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WpfTeamProjectTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WpfTeamProjectTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("18bf9015-b903-480f-99c2-c2a414cac8b0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WpfTeamProjectTest/UserRegistrationTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WpfTeamProject;
+
+namespace WpfTeamProjectTest
+{
+    [TestClass]
+    public class UserRegistrationTest
+    {
+        [TestMethod]
+        public void IsEmptyTest()
+        {
+            // Arrange
+            WpfTeamProject.ViewModel.UserRegistrationViewModel stringInput = new WpfTeamProject.ViewModel.UserRegistrationViewModel() { UserName = "UserName", FirstName = "Name", LastName = ""};
+            bool expected = true;
+
+            // Act
+            bool actual = stringInput.IsEmpty();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IsNumericInputTest()
+        {
+            // Arrange
+            WpfTeamProject.ViewModel.UserRegistrationViewModel stringInput = new WpfTeamProject.ViewModel.UserRegistrationViewModel() { PhoneNumber = 1};
+            bool expected = false;
+
+            // Act
+            bool actual = stringInput.IsNumericInput(stringInput.PhoneNumber);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
